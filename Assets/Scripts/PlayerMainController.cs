9172437eb9a72a0d357bb5d7db059fee568af902
Dukho Choi <dukhochoi@gmail.com>
2019-05-04 19:51:34 +1000

    Add shot prefab

---
+++ Assets/Scripts/PlayerMainController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary {
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerMainController : MonoBehaviour {
+    private Rigidbody rb;
+
+    [SerializeField] private float speed;
+    public Boundary boundary;
+    public float tilt;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.velocity = movement * speed;
+
+        //constraint
+        rb.position = new Vector3(
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            0,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+            );
+
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+    }
+}
