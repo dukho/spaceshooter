26364be671f6b51847a3408bf798351feedae539
Dukho Choi <dukhochoi@gmail.com>
2019-05-04 21:35:48 +1000

    Add firing

--- Assets/Scripts/PlayerMainController.cs
+++ Assets/Scripts/PlayerMainController.cs
@@ -14,8 +14,21 @@ public class PlayerMainController : MonoBehaviour {
     public Boundary boundary;
     public float tilt;
 
+    public GameObject shot;
+    public Transform shotSpawn;
+
+    public float fireRate = 0.5F;
+    private float nextFire = 0.0F;
+
     private void Start() {
         rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update() {
+        if (Input.GetButton("Fire1") && Time.time > nextFire) {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
     }
 
     private void FixedUpdate() {
